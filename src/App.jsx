f06ecf8094ac488bceaa3bb3762be139f430e0ae
Braian  Boianelli <braianboianelli3@gmail.com>
2025-05-21 06:40:06 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import BoostrapNavbar from './components/navbar'
+function App() {
+
+  return (
+    <>
+    <BoostrapNavbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import { Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
+
+export default function BoostrapNavbar() {
+  return (
+    <Navbar bg="light" variant="ligth" expand="lg">
+      <Container fluid={true}>
+        <Navbar.Brand style={{ width: '10rem' }}>
+          <Card.Img href="#home"  src="/mercurio_sin_fondo.png" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbar-collapse' />
+        <Navbar.Collapse id='navbar-collapse' className='justify-content-end color-black'>
+        <Nav>
+          <Form  className='d-flex'>
+            <Form.Control/>
+            <Button variant="outline-dark space-left" >Search</Button>
+          </Form>
+          <Nav.Link className='text-black' href="#home" >Home</Nav.Link>
+          <Nav.Link className='text-black' href="/ofertas">Ofertas</Nav.Link>
+          <Nav.Link className='text-black' href="/backoffice">Administración</Nav.Link>
+          <Nav.Link><i className="bi bi-cart text-black size-[2rem]"></i></Nav.Link>          
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
